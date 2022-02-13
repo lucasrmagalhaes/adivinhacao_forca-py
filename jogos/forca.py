c82ecc7fbd6f8f286ab526d36c758b11894a87a1
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-02-13 11:29:14 -0300

    Criando mais funções

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,27 +3,21 @@ import random
 
 def jogar():
     imprime_mensagem_abertura()
+
     palavra_secreta = carrega_palavra_secreta()
 
     letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    print(letras_acertadas)
 
     enforcou = False
     acertou = False
     erros = 0
 
-    print(letras_acertadas)
-
     while not enforcou and not acertou:
-        chute = input("Qual letra? ")
-        chute = chute.strip().upper()
+        chute = pede_chute()
 
         if chute in palavra_secreta:
-            index = 0
-
-            for letra in palavra_secreta:
-                if chute == letra:
-                    letras_acertadas[index] = letra
-                index += 1
+            marca_chute_correto(palavra_secreta, chute, letras_acertadas)
         else:
             erros += 1
             print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
@@ -34,11 +28,9 @@ def jogar():
         print(letras_acertadas)
 
     if acertou:
-        print("Você ganhou!")
+        imprime_mensagem_vencedor()
     else:
-        print("Você perdeu.")
-
-    print("Fim do Jogo")
+        imprime_mensagem_perdedor()
 
 
 def imprime_mensagem_abertura():
@@ -62,7 +54,31 @@ def carrega_palavra_secreta():
 
 
 def inicializa_letras_acertadas(palavra_secreta):
-    return ["_" for letra in palavra_secreta]
+    return ["_" for _ in palavra_secreta]
+
+
+def pede_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+
+    return chute
+
+
+def marca_chute_correto(palavra_secreta, chute, letras_acertadas):
+    index = 0
+
+    for letra in palavra_secreta:
+        if chute == letra:
+            letras_acertadas[index] = letra
+        index += 1
+
+
+def imprime_mensagem_vencedor():
+    print("Você ganhou!")
+
+
+def imprime_mensagem_perdedor():
+    print("Você perdeu.")
 
 
 if __name__ == "__main__":
